3eb8bbd5aeca0ba774ba60809cf4c7d5eb07a716
Zaki Manian <zaki@iqlusion.io>
2024-05-13 12:18:31 -0400

    Add ScheduleRequest

---
+++ src/lib/ScheduleRequest.svelte
@@ -0,0 +1,55 @@
+<script>
+    import {
+        cellarId,
+        blockHeight,
+        chainId,
+        deadline,
+    } from "$stores/scheduleRequestStore";
+</script>
+
+<h1>Schedule Request</h1>
+
+<div>
+    <label for="cellar_id" title="Enter the cellar id as a string."
+        >Cellar ID:</label
+    >
+    <input
+        type="text"
+        id="cellar_id"
+        bind:value={$cellarId}
+        placeholder="Cellar ID"
+    />
+</div>
+<div>
+    <label for="block_height" title="Enter the block height as a string."
+        >Block Height:</label
+    >
+    <input
+        type="text"
+        id="block_height"
+        bind:value={$blockHeight}
+        placeholder="Block Height"
+    />
+</div>
+<div>
+    <label for="chain_id" title="Enter the chain id as a string."
+        >Chain ID:</label
+    >
+    <input
+        type="text"
+        id="chain_id"
+        bind:value={$chainId}
+        placeholder="Chain ID"
+    />
+</div>
+<div>
+    <label for="deadline" title="Enter the deadline as a boolean."
+        >Deadline:</label
+    >
+    <input
+        type="text"
+        id="deadline"
+        bind:value={$deadline}
+        placeholder="Deadline"
+    />
+</div>
---
+++ src/stores/scheduleRequestStore.js
@@ -0,0 +1,6 @@
+import { writable } from "svelte/store";
+
+export const cellarId = writable("");
+export const blockHeight = writable("");
+export const chainId = writable("");
+export const deadline = writable("");
