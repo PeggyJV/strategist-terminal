d5b8d0410aeb2e23cb277f696b0cf864ec431422
aleots <aleots@ttu.ee>
2024-07-18 09:43:23 +0300

    First adaptor styles

--- src/lib/adaptors/AaveV3ATokenV1.svelte
+++ src/lib/adaptors/AaveV3ATokenV1.svelte
@@ -73,101 +73,131 @@
 
 <!-- Aave V3 Deposit -->
 
-<h1>1. Aave V3 Deposit</h1>
-<div>
-    <label
-        for="token_deposit"
-        title="Enter the ERC-20 token contract address as a string."
-        >ERC-20 Token Contract Address:</label
-    >
-    <input type="text" id="token_deposit" bind:value={token_deposit} placeholder="0xtoken" />
-</div>
-<div>
-    <label
-        for="amount_deposit"
-        title="Enter the amount of the ERC-20 asset to deposit as a string."
-        >Amount of ERC-20 Asset:</label
-    >
-    <input type="text" id="amount_deposit" bind:value={amount_deposit} placeholder="Amount" />
-</div>
-<button on:click={scheduleDeposit}>Deposit</button>
+<div class="prose mt-10 w-screen">
+    <h1>AaveV3ATokenV1</h1>
+    <h2>1. Aave V3 Deposit</h2>
+    <div class="flex justify-between">
+        <label
+            for="token_deposit"
+            title="Enter the ERC-20 token contract address as a string."
+            >ERC-20 Token Contract Address:</label
+        >
+        <input
+          type="text"
+          id="token_deposit"
+          bind:value={token_deposit}
+          placeholder="0xtoken"
+          class="w-60 px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500"
+        />
+    </div>
+    <div class="flex justify-between">
+        <label
+            for="amount_deposit"
+            title="Enter the amount of the ERC-20 asset to deposit as a string."
+            >Amount of ERC-20 Asset:</label
+        >
+        <input
+          type="text"
+          id="amount_deposit"
+          bind:value={amount_deposit}
+          placeholder="Amount"
+          class="w-60 px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500"
+        />
+    </div>
+    <button
+      on:click={scheduleDeposit}
+      class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600 mt-3"
+    >Deposit</button>
 
-<!-- Aave V3 Withdraw -->
+    <!-- Aave V3 Withdraw -->
 
-<h1>2. Aave V3 Withdraw</h1>
-<div>
-    <label
-        for="token_withdraw"
-        title="Enter the ERC-20 token contract address as a string."
-        >ERC-20 Token Contract Address:</label
+    <h2>2. Aave V3 Withdraw</h2>
+    <div class="flex justify-between">
+        <label
+            for="token_withdraw"
+            title="Enter the ERC-20 token contract address as a string."
+            >ERC-20 Token Contract Address:</label
+        >
+        <input
+          type="text"
+          id="token_withdraw"
+          bind:value={token_withdraw}
+          placeholder="0xtoken"
+          class="w-60 px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500"
+        />
+    </div>
+    <div class="flex justify-between">
+        <label
+            for="amount_withdraw"
+            title="Enter the amount of the ERC-20 asset to withdraw as a string."
+            >Amount of ERC-20 Asset:</label
+        >
+        <input
+          type="text"
+          id="amount_withdraw"
+          bind:value={amount_withdraw}
+          placeholder="Amount"
+          class="w-60 px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500"
+        />
+    </div>
+    <button
+      on:click={scheduleWithdraw}
+      class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600 mt-3"
+    >Withdraw</button>
+
+    <!-- Adjust Isolation Mode Asset As Collateral -->
+
+    <h2>3. Adjust Isolation Mode Asset As Collateral</h2>
+    <div class="flex justify-between">
+        <label
+            for="asset"
+            title="Enter the ERC-20 token contract address as a string."
+            >ERC-20 Token Contract Address:</label
+        >
+        <input
+          type="text"
+          id="asset"
+          bind:value={asset}
+          placeholder="0xtoken"
+          class="w-60 px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500"
+        />
+    </div>
+    <div class="flex justify-between">
+        <label for="use_as_collateral" title="Whether to use the asset as collateral."
+            >Use as Collateral:</label
+        >
+        <input
+            type="text"
+            id="use_as_collateral"
+            bind:value={use_as_collateral}
+            placeholder="boolean"
+            class="w-60 px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500"
+        />
+    </div>
+    <button
+      on:click={AdjustIsolationModeAssetAsCollateral}
+      class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600 mt-3"
+        >Adjust</button
     >
-    <input type="text" id="token_withdraw" bind:value={token_withdraw} placeholder="0xtoken" />
-</div>
-<div>
-    <label
-        for="amount_withdraw"
-        title="Enter the amount of the ERC-20 asset to withdraw as a string."
-        >Amount of ERC-20 Asset:</label
-    >
-    <input type="text" id="amount_withdraw" bind:value={amount_withdraw} placeholder="Amount" />
-</div>
-<button on:click={scheduleWithdraw}>Withdraw</button>
 
-<!-- Adjust Isolation Mode Asset As Collateral -->
+    <!-- Change EMode -->
 
-<h1>3. Adjust Isolation Mode Asset As Collateral</h1>
-<div>
-    <label
-        for="asset"
-        title="Enter the ERC-20 token contract address as a string."
-        >ERC-20 Token Contract Address:</label
-    >
-    <input type="text" id="asset" bind:value={asset} placeholder="0xtoken" />
-</div>
-<div>
-    <label for="use_as_collateral" title="Whether to use the asset as collateral."
-        >Use as Collateral:</label
-    >
-    <input
-        type="text"
-        id="use_as_collateral"
-        bind:value={use_as_collateral}
-        placeholder="boolean"
-    />
-</div>
-<button on:click={AdjustIsolationModeAssetAsCollateral}
-    >Adjust Isolation Mode Asset As Collateral</button
->
+    <h2>4. Change EMode</h2>
+    <div class="flex justify-between">
+        <label for="category_id" title="Enter the category_id as a uint32"
+            >Category ID:</label
+        >
+        <input
+            type="text"
+            id="category_id"
+            bind:value={category_id}
+            placeholder="ID"
+            class="w-60 px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500"
+        />
+    </div>
+    <button
+      on:click={ChangeEMode}
+      class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600 mt-3"
+    >Change</button>
 
-<!-- Change EMode -->
-
-<h1>4. Change EMode</h1>
-<div>
-    <label for="category_id" title="Enter the category_id as a uint32"
-        >Category ID:</label
-    >
-    <input
-        type="text"
-        id="category_id"
-        bind:value={category_id}
-        placeholder="ID"
-    />
-</div>
-<button on:click={ChangeEMode}>ChangeEMode</button>
-
-<!-- Additional text information at the bottom of the page -->
-<div>
-    <h2>Specific Strategist Information</h2>
-    <p>
-        A. Keep in mind regarding isolated markets. If they are isolated, then
-        they may not be usable as collateral for other borrow positions.
-    </p>
-    <p>
-        B. See the <a
-            href="https://sommelier-finance.gitbook.io/sommelier-documentation/smart-contracts/protocol-v2-contract-architecture"
-            >docs</a
-        > for more information pertaining to this adaptor.
-    </p>
-    <!-- Empty lines of spaces -->
-    <br /><br /><br />
 </div>
