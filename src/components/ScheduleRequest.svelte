2c00361955922931f22ad284fdff5d18cbf8556a
otsalex <alexots13@gmail.com>
2024-07-29 19:22:10 +0300

    Remove unnecessary inputs and use local variables

--- src/components/ScheduleRequest.svelte
+++ src/components/ScheduleRequest.svelte
@@ -1,19 +1,15 @@
 <script lang="ts">
     import { invoke } from "@tauri-apps/api/tauri";
-    import { cellarId, blockHeight, chainId, deadline } from "$stores/scheduleRequestStore";
     import { queue } from "$stores/AdapterQueue";
     import StateModal from "../components/StateModal.svelte";
-    import Cellars from "$lib/cellars"
+    import Cellars, { type Cellar, Chains } from "$lib/cellars"
 
     let modalVisible = false;  
     let showTooltip = false;
 
-    const chainIdMap = {
-        Ethereum: "1",
-        Arbitrum: "42161",
-        Optimism: "10",
-        Scroll: "534353"
-    }
+    let cellar: Cellar = Cellars.REAL_YIELD_ETH;
+    let blockHeight = "";
+    let deadline = "";
 
     function toggleModal() {
         modalVisible = !modalVisible;
@@ -22,13 +18,13 @@
     async function scheduleRequest() {
         let calls = $queue.map((call) => call.json_fields());
 
-        const deadlineDate = new Date($deadline);
-        const deadlineUnixTimestamp = Math.floor(deadlineDate.getTime() / 1000);
+        const deadlineDate = new Date(deadline);
+        const deadlineUnixTimestamp = Math.floor(deadlineDate.getTime() / 1000) || "";
 
         const result = await invoke("schedule_request", {
-            cellarId: $cellarId,
-            blockHeight: $blockHeight,
-            chainId: $chainId,
+            cellarId: cellar.ADDRESS,
+            blockHeight: blockHeight,
+            chainId: cellar.CHAIN.chainId,
             deadline: deadlineUnixTimestamp,
             queue: calls,
         }).then(result => {
@@ -41,41 +37,28 @@
         queue.set([]);
     }
 
-    $: isButtonEnabled = $cellarId.trim().length > 0 && 
-                         $blockHeight.trim().length > 0 &&
-                         $chainId.trim().length > 0 &&
-                         $deadline.trim().length > 0;
+    $: isButtonEnabled = blockHeight.trim().length > 0
 </script>
 
 <h1 class="text-2xl font-bold mb-4">Schedule Request</h1>
 <div class="mb-4">
     <label for="cellar_id" class="block mb-1">Cellar:</label>
 
-    <select name="cellar_id" id="cellar_id" bind:value={$cellarId} class="w-full px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500">
+    <select name="cellar_id" id="cellar_id" bind:value={cellar} class="w-full px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500">
         {#each Object.entries(Cellars) as [key, value]}
-            <option value={value.ADDRESS}>{key}</option>
+            <option value={value}>{key}</option>
         {/each}
     </select>
  </div>
 <div class="mb-4">
     <label for="block_height" class="block mb-1">Block Height:</label>
-    <input type="text" id="block_height" class="w-full px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500" bind:value={$blockHeight} placeholder="Enter Block Height"/>
-</div>
-<div class="mb-4">
-
-    <label for="chain_id" class="block mb-1">Chain:</label>
-
-    <select name="chain_id" id="chain_id" bind:value={$chainId} class="w-full px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500">
-        {#each Object.entries(chainIdMap) as [key, value]}
-            <option value={value}>{key}</option>
-        {/each}
-    </select>
+    <input type="text" id="block_height" class="w-full px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500" bind:value={blockHeight} placeholder="Enter Block Height"/>
 </div>
 
-{#if $chainId !== "1"}
+{#if cellar.CHAIN !== Chains.ETHEREUM}
     <div class="mb-4">
         <label for="deadline" class="block mb-1">Deadline:</label>
-        <input type="datetime-local" id="deadline" class="w-full px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500" bind:value={$deadline} placeholder="Enter Deadline"/>
+        <input type="datetime-local" id="deadline" class="w-full px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500" bind:value={deadline} placeholder="Enter Deadline"/>
     </div>
 {/if}
 
