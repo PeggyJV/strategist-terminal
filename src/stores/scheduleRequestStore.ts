851949cd9245dd31a84a7521a2237c205a6d4f3f
otsalex <alexots13@gmail.com>
2024-07-29 19:22:45 +0300

    Remove unused store

--- src/stores/scheduleRequestStore.ts
+++
@@ -1,6 +0,0 @@
-import { writable, type Writable } from "svelte/store";
-
-export const cellarId: Writable<string> = writable("");
-export const blockHeight: Writable<string> = writable("");
-export const chainId: Writable<string> = writable("");
-export const deadline: Writable<string> = writable("");
