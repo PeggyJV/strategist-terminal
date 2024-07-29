d4dfad203ebb5b55e0bbd9d432815b0eb4fd1d42
otsalex <alexots13@gmail.com>
2024-07-29 18:26:46 +0300

    Add placeholder and improve style

--- src/components/AdaptorSelection.svelte
+++ src/components/AdaptorSelection.svelte
@@ -24,34 +24,33 @@
   }
 
 </script>
+<div class="border-2 rounded-2xl p-5">
+  {#if adaptorListVisible === true}
+    <h2>Select adaptor:</h2>
+    <div class="mb-2 w-full max-w-md">
+      <input
+        type="text"
+        bind:value={searchQuery}
+        placeholder="Search adaptors..."
+        class="w-full px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500"
+      />
+    </div>
 
-{#if adaptorListVisible === true}
-  <h2>Select adaptor:</h2>
-  <div class="mb-2 w-full max-w-md">
-    <input
-      type="text"
-      bind:value={searchQuery}
-      placeholder="Search adaptors..."
-      class="w-full px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500"
-    />
-  </div>
+    <div class="flex flex-wrap gap-2.5 mt-12 justify-center">
+      {#each filteredAdaptors as adaptor}
+        <button
+          on:click={selectAdaptor}
+          class="p-2.5 border rounded focus:outline-none bg-gray-100 text-black border-gray-300"
+        >{adaptor.name}</button>
+      {/each}
+    </div>
+  {/if}
 
-  <div class="flex flex-wrap gap-2.5 mt-12 justify-center">
-    {#each filteredAdaptors as adaptor}
-      <button
-        on:click={selectAdaptor}
-        class="p-2.5 border rounded focus:outline-none bg-gray-100 text-black border-gray-300"
-      >{adaptor.name}</button>
-    {/each}
-  </div>
-{/if}
-
-{#if selectedAdaptor}
-  <h2>Select adaptor call:</h2>
-  <CallSelection
-    adaptor={selectedAdaptor}
-    closeAdaptorSelection={closeAdaptorSelection}/>
-{/if}
-
+  {#if selectedAdaptor}
+    <CallSelection
+      adaptor={selectedAdaptor}
+      closeAdaptorSelection={closeAdaptorSelection}/>
+  {/if}
+</div>
 
 
