aa2659c2a8125245fae76501e6e0c2fc28bf53a1
Collin Brittain <collinbrittain@gmail.com>
2024-08-22 15:10:49 -0500

    Improve log formating

--- src-tauri/src/logging.rs
+++ src-tauri/src/logging.rs
@@ -3,6 +3,7 @@ use std::{collections::HashMap, fmt::Arguments};
 use fern::FormatCallback;
 use log::Record;
 
+/// A visitor that collects key-value pairs from a log record for display.
 #[derive(Debug, Default)]
 pub(crate) struct KVVisitor {
     pub map: HashMap<String, String>,
@@ -10,10 +11,19 @@ pub(crate) struct KVVisitor {
 
 impl std::fmt::Display for KVVisitor {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self.map)
+        let mut lines = Vec::<String>::new();
+        for (key, value) in &self.map {
+            lines.push(format!("    {}: {}", key, value));
+        }
+
+        let output = lines.join("\n");
+
+        write!(f, "{}", output)
     }
 }
 
+/// Implement the `VisitSource` trait for the `KVVisitor` struct in order to read the key-value
+/// pairs from a log record.
 impl<'kvs> log::kv::VisitSource<'kvs> for KVVisitor {
     fn visit_pair(
         &mut self,
@@ -21,6 +31,7 @@ impl<'kvs> log::kv::VisitSource<'kvs> for KVVisitor {
         value: log::kv::Value<'kvs>,
     ) -> Result<(), log::kv::Error> {
         self.map.insert(key.to_string(), value.to_string());
+
         Ok(())
     }
 }
@@ -28,15 +39,27 @@ impl<'kvs> log::kv::VisitSource<'kvs> for KVVisitor {
 // The formatting function used by the log tauri plugin
 pub(crate) fn format_log(out: FormatCallback, message: &Arguments, record: &Record) {
     let mut key_values = KVVisitor::default();
-
     record.key_values().visit(&mut key_values).unwrap();
 
+    // Format differently depending on if any key-value pairs are included in the record
+    if key_values.map.len() > 0 {
+        out.finish(format_args!(
+            "{} {} [{}] {} \n{}",
+            chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+            record.level(),
+            record.target(),
+            message,
+            key_values,
+        ));
+
+        return;
+    }
+
     out.finish(format_args!(
-        "[{}] [{}] [{}] {} {}",
+        "{} {} [{}] {}",
         chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
-        record.target(),
         record.level(),
+        record.target(),
         message,
-        key_values,
     ))
 }
