0bc35c38da64ef459a57bbb768b64255b0f92af9
otsalex <alexots13@gmail.com>
2024-10-10 15:08:20 +0300

    Removing calls from queue

--- src/components/Queue.svelte
+++ src/components/Queue.svelte
@@ -1,20 +1,33 @@
 <script lang="ts">
-  import { queue } from "$stores/AdapterQueue";
+  import { CellarCall, queue } from "$stores/AdapterQueue"
 
   function clearQueue() {
     queue.set([]);
+  }
+  function removeCall(index: number) {
+    queue.update((calls: CellarCall[]) => {
+      return calls.filter((_, i) => i !== index);
+    });
   }
 
 </script>
 <div class="prose mt-10">
   <h2 class="text-2xl font-bold mb-4">Queue:</h2>
-  <div class="max-w-4xl mx-auto">
+  <div class="max-w-4xl min-w-[250px] mx-auto">
     <div class="bg-white shadow-md rounded-lg overflow-hidden">
 
       <div class="px-4 py-2">
-        {#each $queue as item}
+        {#each $queue as item, index}
           <div class="mb-4">
-            <h2 class="text-xl font-bold not-prose">{item.adaptorName}</h2>
+            <div class="flex flex-row">
+              <h2 class="text-xl font-bold mt-2 not-prose">{item.adaptorName}</h2>
+              <button
+                on:click={() => removeCall(index)}
+                type="button"
+                class="px-1 text-red-500 text-3xl rounded-md hover:text-red-600 "
+              > &times;</button>
+            </div>
+
             {#each Object.entries(item.fields) as [key, value]}
               <div class="mb-2">
                 <p class="font-bold">{key}:</p>
@@ -23,6 +36,7 @@
                 </pre>
               </div>
             {/each}
+
           </div>
         {/each}
       </div>
